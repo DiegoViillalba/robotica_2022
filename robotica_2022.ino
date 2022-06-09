0ebda10d768ab9adea60b7faee9d8b990c0fb906
EllechesxD <107021731+EllechesxD@users.noreply.github.com>
2022-06-09 00:48:26 -0500

    Codigo con funciób
    a

--- robotica_2022.ino
+++ robotica_2022.ino
@@ -1,24 +1,24 @@
 /*
 --------------------------------------------- AUTORES ---------------------------------------------
-                                    Hernández Cuevas Jonathan Alexis 
-                                      Villalba Gónzalez Diego Antonio
+                              Hernández Cuevas Jonathan Alexis 
+                               Villalba Gónzalez Diego Antonio
 */
 
-/*-------------------------DECLARAMOS VARIABLES NECESARIAS PARA EL CÓDIGO-------------------------*/
-int motor; //Declaramos la variable motores del tipo entero 
-int sensor; //Declaramos la variable sensor del tipo entero 
+/*-------------------------DECLARAMOS VARIABLES NECESARIAS PARA EL CÓDIGO------------------------- aa*/
 int i; //Declaramos la variable i del tipo entero para asignar los pines establecidos
 int potencia; //Declaramos la variable potencia del tipo entero para la velocidad de los motores
 char memoria; //Declaramos la variable memoria del tipo entero para el caso donde esta en blanco (Guardar)
-bool s1 = 0; //Declaramos la variable s1 para la lectura de valores 
-bool s2 = 0; //Declaramos la variable s2 para la lectura de valores
-bool s3 = 0; //Declaramos la variable s3 para la lectura de valores
-bool d = 0; //Declaramos la variable d para la lectura de valores
-bool l = 0; //Declaramos la variable i para la lectura de valores
+int s1 = 0; //Declaramos la variable s1 para la lectura de valores 
+int s2 = 0; //Declaramos la variable s2 para la lectura de valores
+int s3 = 0; //Declaramos la variable s3 para la lectura de valores
 bool button; //Declaramos la variable button para la lectura de valores
 bool sentido = 0;
+int s1_r = 0; //Declaramos la variable s1 para la lectura de valores 
+int s2_r = 0; //Declaramos la variable s2 para la lectura de valores
+int s3_r = 0; //Declaramos la variable s3 para la lectura de valores
 
 /*-------------------------DECLARAMOS ASPECTOS DEL CÓDIGO-------------------------*/
+
 void setup() 
 {
   Serial.begin(9600); //Inicializamos comunicación serial
@@ -30,6 +30,20 @@ void setup()
   pinMode(A6, INPUT); //Declaramos A5 como salida al push button
   pinMode(6, OUTPUT); //Declaramos A2 como salida al Motor Derecha
   pinMode(5, OUTPUT); //Declaramos A2 como salida al Motor Izquierda
+}
+
+/*-------------------------DEFINIMOS LA FUNCION PARA NUESTROS LECTURA-------------------------*/
+int funcionp(int s1, int s2, int s3)
+{
+  float p; //Declaramos variables para nuestras funciones 
+  float d; //Declaramos variables para nuestras funciones 
+  float v; //Declaramos variables para nuestras funciones 
+  float k; //Declaramos variables para nuestras funciones 
+  p = (s1+s3)/2; 
+  d = (s2-p);
+  k = d*.30;
+  v = s2 - k;
+  return v;
 }
 
 /*-------------------------DEFINIMOS LA FUNCION PARA NUESTROS MOTORES-------------------------*/
@@ -58,242 +72,111 @@ void atras(){
   }
 //FUNCION QUE INDICA EL MOVIMIENTO DE LOS MOTORES: DERECHA
 void derecha(){
-    Motor1(i=0, potencia = 255);
-    Motor2(i=0, potencia = 100);
+    Motor1(i=0, potencia = 0);
+    Motor2(i=0, potencia = 255);
   }
-//FUNCION QUE INDICA EL MOVIMIENTO DE LOS MOTORES: DERECHA_R
-void derecha_r(){
-    Motor1(i=0, potencia = 255);
-    Motor2(i=0, potencia = 100);
-    delay(500);
-    Motor1(i=0, potencia = 255);
-    Motor2(i=0, potencia = 20);
-  }
+
 //FUNCION QUE INDICA EL MOVIMIENTO DE LOS MOTORES: IZQUIERDA
 void izquierda(){
-    Motor1(i=0, potencia = 100);
-    Motor2(i=0, potencia = 255);
+    Motor1(i=0, potencia = 255);
+    Motor2(i=0, potencia = 0);
   }
-//FUNCION QUE INDICA EL MOVIMIENTO DE LOS MOTORES: IZQUIERDA_R
-void izquierda_r(){
-    Motor1(i=0, potencia = 100);
-    Motor2(i=0, potencia = 255);
-    delay(500);
-    Motor1(i=0, potencia = 20);
-    Motor2(i=0, potencia = 255);
-  }
+
 //FUNCION QUE INDICA EL MOVIMIENTO DE LOS MOTORES: DETENIDO
 void alto(){
     Motor1(i=2, potencia = 255);
     Motor2(i=2, potencia = 255);
   }
 
+// Funcion de lectuira de blanco y negro
+
+
+
 //DECLARAMOS INSTRUCCIONES DEL CÓDIGO
 void loop() {
-  button = analogRead(A6);
-  
-  if(button == 1){
+  button = digitalRead(A6);
+  float v;
+
       //ASIGNAMOS VALORES A NUESTRAS VARIABLES
-    s1 = digitalRead(A4); //Asignamos a la variable s1 la lectura del sensor reflexivo conectado al pin A4
-    s2 = digitalRead(A3); //Asignamos a la variable s2 la lectura del sensor reflexivo conectado al pin A3
-    s3 = digitalRead(A2); //Asignamos a la variable s3 la lectura del sensor reflexivo conectado al pin A2
-    d = digitalRead(A1); //Asignamos a la variable s3 la lectura del sensor reflexivo conectado al pin d
-    l = digitalRead(A5); //Asignamos a la variable s3 la lectura del sensor reflexivo conectado al pin i
+    s2_r = analogRead(A3); //Asignamos a la variable s2 la lectura del sensor reflexivo conectado al pin A3
+    s3_r = analogRead(A2); //Asignamos a la variable s3 la lectura del sensor reflexivo conectado al pin A2
+    s1_r = analogRead(A4); //Asignamos a la variable s1 la lectura del sensor reflexivo conectado al pin A4
+    // d = digitalRead(A1); //Asignamos a la variable s3 la lectura del sensor reflexivo conectado al pin d
+    // l = digitalRead(A5); //Asignamos a la variable s3 la lectura del sensor reflexivo conectado al pin i
     /*
                                     REGISTRO DE LECTURA DE LOS SENSORES
                                         1=Blanco       0=Negro
     */
 
-    //IMPRIMIMOS EN PANTALLA LO QUE LEEN NUESTROS SENSORERES
-    Serial.println("Sensor izquierda    Sensor Centro     Sensor Derecha");//Imprimimos texto en pantalla
-    Serial.print(s1);//Imprimimos valor en pantalla
-    Serial.print("                     ");//Espacio
-    Serial.print(s2);//Imprimimos valor en pantalla
-    Serial.print("                     ");//Espacio
-    Serial.println(s3);//Imprimimos valor en pantalla
+    /*
+      IMPRIMIMOS EN PANTALLA LO QUE LEEN NUESTROS SENSORERES
+      Serial.println("Sensor izquierda    Sensor Centro     Sensor Derecha");//Imprimimos texto en pantalla
+      Serial.print(s1);//Imprimimos valor en pantalla
+      Serial.print("                     ");//Espacio
+      Serial.print(s2);//Imprimimos valor en pantalla
+      Serial.print("                     ");//Espacio
+      Serial.println(s3);//Imprimimos valor en pantalla
+    */
+  // v = lec(s1_r,s2_r,s3_r);
+  //v = 500;
 
+  if (s1_r <= v)
+  {
+    s1 = 0;
+    Serial.println(s1_r);//Espacio
+  }
+  else if(s1_r>v)
+  {
+    s1=1;
+    Serial.println(s1_r);//Espacio
+  }
+  if (s2_r <= v)
+  {
+    s2 = 0;
+  }
+  else if(s2_r>v)
+  {
+    s2=1;
+  }
+
+  if (s3_r <= v)
+  {
+    s3 = 0;
+  }
+  else if(s3_r>v)
+  {
+    s3=1;
+  }
 
     //HACEMOS LOS CASOS PARA EL MOVIMIENTO DE NUESTRO CARRO
 
-    if((l==0) && (s1==0) && (s2==0) && (s3==0) && (d==0))
+    if( (s1==0) && (s2==0) && (s3==0))
     {
-        alto();//Llamamos a la funcion 
-        memoria='0';//Guardamos los valores en la variable para que asosie al movimieto
-      }else if((l==0) && (s1==0) && (s2==1) && (s3==0) && (d==0))
-      {
-        adelante();//Llamamos a la funcion 
-        memoria='1'; //Guardamos los valores en la variable para que asosie al movimieto
-      }else if((l==0) && (s1==1) && (s2==0) && (s3==0) && (d==0))
-      {
+      if(memoria == '2'){
         izquierda();//Llamamos a la funcion 
+        Serial.println("izquierda");//Espacio
         memoria='2';//Guardamos los valores en la variable para que asosie al movimieto
-      }else if((l==0) && (s1==0) && (s2==0) && (s3==1) && (d==0))
-      {
+      }
+          
+      else if(memoria == '3'){
         derecha();//Llamamos a la funcion 
+        Serial.println("derecha");//Espacio
         memoria='3';//Guardamos los valores en la variable para que asosie al movimieto
       }
-      else if ((l==1) && (s1==1) && (s2==0) && (s3==0) && (d==0))
-      {
-      izquierda_r();//Llamamos a la funcion 
-      memoria='4';//Guardamos los valores en la variable para que asosie al movimieto
-      }
-      else if ((l==0) && (s1==0) && (s2==0) && (s3==1) && (d==1))
-      {
-      derecha_r();//Llamamos a la funcion 
-      memoria='5';//Guardamos los valores en la variable para que asosie al movimieto
-      }
+    }else if( (s1==0) && (s2==1) && (s3==0))
+    {
+      adelante();//Llamamos a la funcion 
+      Serial.println("adelante");//Espacio
+      memoria='1'; //Guardamos los valores en la variable para que asosie al movimieto
+    }else if( (s1==1) && (s2==0) && (s3==0))
+    {
+      izquierda();//Llamamos a la funcion 
+      Serial.println("izquierda");//Espacio
+      memoria='2';//Guardamos los valores en la variable para que asosie al movimieto
+    }else if( (s1==0) && (s2==0) && (s3==1))
+    {
+      derecha();//Llamamos a la funcion 
+      Serial.println("derecha");//Espacio
+      memoria='3';//Guardamos los valores en la variable para que asosie al movimieto
     }
 }
-
-
-
-
-
-
-
-
-
-/*
-                    IDEA DOS PARA EL CODIGO DEL SEGUIDOR DE LINEA (CAMBIA SOLO EL LOOP)
-//Si se lleva acabo la segunda idea es necesario declarar dos variables globales extra llamadas:
-//int bandera1 = 0;
-//int bandera2 = 1;
-
-//Para la segunda idea se necesita redefiinir las funciones de los motores de la siguiente manera
-
--------------------------DEFINIMOS LA FUNCION PARA NUESTROS MOTORES-------------------------
-
-//MOTOR 1
- void Motor1(int i, int potencia)
-  {
-    analogWrite(5,potencia); //Se habilita el motor 1 
-  }
-
-//MOTOR 2
-void Motor2(int i, int potencia)
-  {
-    analogWrite(6,potencia); //Se habilita el motor 1 
-  }
-  
-//FUNCION QUE INDICA EL MOVIMIENTO DE LOS MOTORES: ADELANTE
-void adelante(int potencia){
-    Motor1(i=0, potencia);
-    Motor2(i=0, potencia);
-  }
-//FUNCION QUE INDICA EL MOVIMIENTO DE LOS MOTORES: ATRÁS
-void atras(){
-    Motor1(i=1, potencia);
-    Motor2(i=1, potencia);
-  }
-//FUNCION QUE INDICA EL MOVIMIENTO DE LOS MOTORES: DERECHA
-void derecha(){
-    Motor1(i=0, potencia);
-    Motor2(i=0, potencia);
-  }
-//FUNCION QUE INDICA EL MOVIMIENTO DE LOS MOTORES: IZQUIERDA
-void izquierda(){
-    Motor1(i=0, potencia);
-    Motor2(i=0, potencia;
-  }
-//FUNCION QUE INDICA EL MOVIMIENTO DE LOS MOTORES: DETENIDO
-void alto(){
-    Motor1(i=2, potencia);
-    Motor2(i=2, potencia);
-  }
-
-void loop() {
-  
-  //ASIGNAMOS VALORES A NUESTRAS VARIABLES
-  s1 = digitalRead(A4); //Asignamos a la variable s1 la lectura del sensor reflexivo conectado al pin A4
-  s2 = digitalRead(A3); //Asignamos a la variable s2 la lectura del sensor reflexivo conectado al pin A3
-  s3 = digitalRead(A2); //Asignamos a la variable s3 la lectura del sensor reflexivo conectado al pin A2
-
-  
-  //                                REGISTRO DE LECTURA DE LOS SENSORES
-  //                                    1=Negro       0=blanco
-  
-
-  //IMPRIMIMOS EN PANTALLA LO QUE LEEN NUESTROS SENSORERES
-  Serial.println("Sensor izquierda    Sensor Centro     Sensor Derecha");//Imprimimos texto en pantalla
-  Serial.print(s1);//Imprimimos valor en pantalla
-  Serial.print("                     ");//Espacio
-  Serial.print(s2);//Imprimimos valor en pantalla
-  Serial.print("                     ");//Espacio
-  Serial.println(s3);//Imprimimos valor en pantalla
-
-
-  //HACEMOS LOS CASOS PARA EL MOVIMIENTO DE NUESTRO CARRO
-  
-  if((s1==0) && (s2==0) && (s3==0))
-  {
-      atras(potencia = 100); //Llamamos a función
-      delay(100); //Hacemos una pausa del tiempo indicado 
-      bandera1 = 0; //Asignamos valor a una variable
-  }
-  else if((s1==0) && (s2==0) && (s3==1))
-  {
-    bandera1 = 1; //Asignamos valor a una variable
-    atras(potencia = 150); //Llamamos a función
-    delay(20); //Hacemos una pausa del tiempo indicado 
-    //REALIZAMOS UN GIRO GRADUALMENTE
-    izquierda(potencia = 100); //Llamamos a función
-    delay(50); //Hacemos una pausa del tiempo indicado 
-    izquierda(potencia = 200); //Llamamos a función
-    delay(50); //Hacemos una pausa del tiempo indicado 
-    izquierda(potencia = 250); //Llamamos a función
-    delay(70); //Hacemos una pausa del tiempo indicado 
-  }
-else if((s1==0) && (s2==1) && (s3==1))
-  {
-    bandera1 = 1; //Asignamos valor a una variable
-    atras(potencia = 150); //Llamamos a función
-    delay(20); //Hacemos una pausa del tiempo indicado 
-    //REALIZAMOS UN GIRO GRADUALMENTE
-    izquierda(potencia = 100); //Llamamos a función
-    delay(50); //Hacemos una pausa del tiempo indicado 
-    izquierda(potencia = 200); //Llamamos a función
-    delay(50); //Hacemos una pausa del tiempo indicado 
-    izquierda(potencia = 250); //Llamamos a función
-    delay(70); //Hacemos una pausa del tiempo indicado 
-  }  
-  else if((s1==1) && (s2==0) && (s3==0))
-  {
-    bandera1 = 1; //Asignamos valor a una variable
-    atras(potencia = 150); //Llamamos a función
-    delay(20); //Hacemos una pausa del tiempo indicado 
-    //REALIZAMOS UN GIRO GRADUALMENTE
-    derecha(potencia = 100); //Llamamos a función
-    delay(50); //Hacemos una pausa del tiempo indicado 
-    derecha(potencia = 200); //Llamamos a función
-    delay(50); //Hacemos una pausa del tiempo indicado 
-    derecha(potencia = 250); //Llamamos a función
-    delay(70); //Hacemos una pausa del tiempo indicado 
-  }
-  else if((s1==1) && (s2==1) && (s3==0))
-  {
-    bandera1 = 1; //Asignamos valor a una variable
-    atras(potencia = 150); //Llamamos a función
-    delay(20); //Hacemos una pausa del tiempo indicado 
-    //REALIZAMOS UN GIRO GRADUALMENTE
-    derecha(potencia = 100); //Llamamos a función
-    delay(50); //Hacemos una pausa del tiempo indicado 
-    derecha(potencia = 200); //Llamamos a función
-    delay(50); //Hacemos una pausa del tiempo indicado 
-    derecha(potencia = 250); //Llamamos a función
-    delay(70); //Hacemos una pausa del tiempo indicado 
-  }
-  else if((s1==1) && (s2==1) && (s3==1))
-  {
-    bandera2 = 1;
-  }
-  if((bandera1 == 1) && (bandera2 == 1)){
-    bandera1 = 0;
-    bandera2 = 0;
-    adelante(potencia = 100); //Llamamos a función
-  }
-  if((bandera1 == 0) && (bandera2 == 1))
-  {
-    adelante(potencia = 110); //Llamamos a función
-  }
-}
-*/
